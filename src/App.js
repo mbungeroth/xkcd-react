f2fadfedf2df2af19ab5ff028341696c16aa4524
MARK BUNGEROTH <mark@MARKs-MacBook-Pro.local>
2018-07-14 09:51:29 -0400

    lift state up from Comic.js to App.js

--- src/App.js
+++ src/App.js
@@ -2,18 +2,41 @@ import React, { Component } from 'react';
 import './App.css';
 import Controls from './Components/Controls';
 import Comic from './Components/Comic';
+import proxiedFetch from 'proxied-fetch';
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      comicURL: '',
+      newest: null
+    }
+  }
+
   render() {
     return (
       <div>
         <h1>xkcd comics</h1>
-        <Controls />
-        <Comic />
+        <Controls
+          newest={this.state.newest}
+        />
+        <Comic
+          comicURL={this.state.comicURL}
+        />
       </div>
     );
   }
+
+  componentDidMount() {
+    proxiedFetch("https://xkcd.com/info.0.json")
+      .then(response => response.json())
+      .then(result => {
+        this.setState({comicURL: result.img,
+                       newest: result.num})
+      })
+      .catch(error => console.log(error));
+  };
 }
 
 export default App;
--- src/Components/Comic.js
+++ src/Components/Comic.js
@@ -1,33 +1,8 @@
-import React, { Component } from 'react';
-import proxiedFetch from 'proxied-fetch';
+import React from 'react';
 
-class Comic extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      comicURL: '',
-      newest: null
-    }
-  }
-
-  render() {
-    return (
-      <div>
-        <img src={this.state.comicURL} alt="comic here"/>
-      </div>
-    )
-  }
-
-  componentDidMount() {
-    proxiedFetch("https://xkcd.com/info.0.json")
-      .then(response => response.json())
-      .then(result => {
-        this.setState({comicURL: result.img,
-                       newest: result.num})
-      })
-      .catch(error => console.log(error));
-  };
-}
-
+const Comic = (props) =>
+  <div>
+    <img src={props.comicURL} alt="there should be a comic here"/>
+  </div>
 
 export default Comic;
