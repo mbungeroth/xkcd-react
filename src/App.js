793a64fe31b608a5b157afb6a5d6418edf948457
mbungeroth <mbungeroth@gmail.com>
2018-07-15 09:45:50 -0400

    add 'previous' button functionality

--- src/App.js
+++ src/App.js
@@ -4,19 +4,36 @@ import Controls from './Components/Controls';
 import Comic from './Components/Comic';
 import proxiedFetch from 'proxied-fetch';
 
+const PATH_BASE = 'https://xkcd.com/';
+const PATH_END = '/info.0.json';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       comicURL: '',
-      newest: null
-    }
+      newest: null,
+      current: null,
+      title: ''
+    };
     this.handleClick = this.handleClick.bind(this);
   }
   handleClick(button) {
     if (button === 'first') {
-      this.setState({comicURL: "https://imgs.xkcd.com/comics/barrel_cropped_(1).jpg"});
+      this.setState({
+        comicURL: 'https://imgs.xkcd.com/comics/barrel_cropped_(1).jpg',
+      });
+    } else if (button === 'previous') {
+      proxiedFetch(`${PATH_BASE}${this.state.current - 1}${PATH_END}`)
+        .then(response => response.json())
+        .then(result => {
+          this.setState({
+            comicURL: result.img,
+            current: result.num,
+            title: result.title
+          });
+        })
+        .catch(error => console.log(error));
     }
   }
 
@@ -24,26 +41,26 @@ class App extends Component {
     return (
       <div>
         <h1>xkcd comics</h1>
-        <Controls
-          newest={this.state.newest}
-          handleClick={this.handleClick}
-        />
-        <Comic
-          comicURL={this.state.comicURL}
-        />
+        <Controls newest={this.state.newest} handleClick={this.handleClick} />
+        <Comic comicURL={this.state.comicURL} />
+        <h6>All comics courtesy of xkcd.com</h6>
       </div>
     );
   }
 
   componentDidMount() {
-    proxiedFetch("https://xkcd.com/info.0.json")
+    proxiedFetch('https://xkcd.com/info.0.json')
       .then(response => response.json())
       .then(result => {
-        this.setState({comicURL: result.img,
-                       newest: result.num})
+        this.setState({
+          comicURL: result.img,
+          newest: result.num,
+          current: result.num,
+          title: result.title
+        });
       })
       .catch(error => console.log(error));
-  };
+  }
 }
 
 export default App;
